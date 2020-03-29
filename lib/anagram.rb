b8e2fc57a933dc99be433a8002bc8b8b9748b436
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-29 15:49:21 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    words.collect{|i| i if i.split("").sort == word.split("").sort}.compact
+  end
+
+end
+
