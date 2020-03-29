1881d63a4a8e410c7f953a80697948f0dceb8eb4
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-29 15:49:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,14 +9,6 @@ class Anagram
   def match(words)
     words.collect{|i| i if i.split("").sort == word.split("").sort}.compact
   end
-  
-  # def is_anagram?(compare)
-  #   words.split(" ").sort.join
-  # end
+
 end
 
-# if %w(words) == %w(word)
-    #   words << self.new
-    # else
-    #   Array.new
-    # end
