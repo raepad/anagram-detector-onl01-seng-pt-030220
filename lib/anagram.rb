b1e2a229cf30c568c0c81f42c5dc4a785d14becd
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-29 15:15:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    words = []
+    words.each do |i|
+      i
+    end
+  end
+  
+end
